72d80bef426e56d0e01df46e94adc04a48e52442
Thaliaio <contatothalitanunes@gmail.com>
2022-10-24 21:37:00 -0300

    :zap: perf: Adicionando um usuário

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Responsável por criar a função de adicionar um usuário
-const usuario = require("./user.json");
+import usuario from "./user.json";
 const adicionarUsuario = (usuario) => {
   const { nome, idade, sexo, email, telefone } = usuario;
 
@@ -9,3 +9,11 @@ const adicionarUsuario = (usuario) => {
 
   return usuarioParaAdicionar;
 };
+
+adicionarUsuario({
+  nome: "Bla Bla",
+  idade: 30,
+  sexo: "F",
+  email: "blabla@gmail.com",
+  telefone: "999999999",
+});
